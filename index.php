b7ce6cff51b04f4b736a3eb6ec9fa2a0896f8ab7
michaelcockle <michael@axisweb.org>
2012-11-17 16:39:38 +0000

    deleted many files

--- index.php
+++ index.php
@@ -10,40 +10,36 @@
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/main.css">
         <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+        <style>
+            div {margin:50px;}
+        </style>
     </head>
     <body>
+<div>
+        <?php $array1 = array(4,8,15,16,23,42); ?>
+
+        Count: <?php echo count($array1); ?><br />
+        Max value: <?php echo max($array1); ?><br />
+        Min value: <?php echo min($array1); ?><br />
+        <br />
+
+        Sort: <?php sort($array1); print_r($array1); ?><br />
+        Reverse Sort: <?php rsort($array1); print_r($array1); ?><br />
+        <br />
+
+        <?php
+            // Implode converts an array into a string using a "join string"
+            // Explode converts a string into an array using a "divide string"
+        ?>
+        Implode: <?php echo $string1 = implode(" * ", $array1); ?><br />
+        Explode: <?php print_r(explode(" * ", $string1)); ?><br />
+        <br />
+        
+        In array: <?php echo in_array(15, $array1); // returns T/F ?><br />
+        
+</div>
 
 
-        <h1><?php echo "Hello " .  " World"; ?></h1>
-
-<p>
-<?php 
-$var1 = 10;
-echo $var1;
-?>
-</p>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.2.min.js"><\/script>')</script>
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
 
